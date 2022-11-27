8bca20a4b01bef3288cb2216a5d02991d880c72b
Arthur mendes <arthurmcunha123@gmail.com>
2022-11-27 17:47:50 -0300

    + Alterações finais flaybird

---
+++ arquivos_iniciais/jogo.js
@@ -0,0 +1,7 @@
+console.log(' Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
