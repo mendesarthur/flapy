827cb1285f6cd954954a8dc2c0c8b17c57e3b24a
Arthur mendes <arthurmcunha123@gmail.com>
2022-11-27 17:48:42 -0300

    + Mudanças finais flapy bird

--- arquivos_iniciais/jogo.js
+++
@@ -1,7 +0,0 @@
-console.log(' Flappy Bird');
-
-const sprites = new Image();
-sprites.src = './sprites.png';
-
-const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
